93937baf3e40b0f1c233a9d0cb75fe1c3b4ace6a
petherin <p_eth@MOXIE>
2017-01-05 20:35:53 +0000

    Added grid columns and LIFF Twitter feed

--- LIFF.Web/App_Start/BundleConfig.cs
+++ LIFF.Web/App_Start/BundleConfig.cs
@@ -11,7 +11,7 @@ namespace LIFF.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bootstrap/js").Include("~/js/bootstrap.js", "~/js/site.js"));
-            bundles.Add(new StyleBundle("~/bootstrap/css").Include("~/css/bootstrap.css", "~/js/site.css"));
+            bundles.Add(new StyleBundle("~/bootstrap/css").Include("~/css/bootstrap.css", "~/css/site.css"));
 
             BundleTable.EnableOptimizations = true;
         }
